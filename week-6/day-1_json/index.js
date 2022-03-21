d65556876ec4827e51c4f441b997711aca9457c9
mutiara-azzahra <my26recovery@gmail.com>
2022-03-21 23:32:28 +0800

    commit 21 maret 22

---
+++ week-6/day-1_json/index.js
@@ -0,0 +1,10 @@
+// JSON : Javasript Object Notation
+
+const obj = {
+  name: "audy",
+};
+
+const stringyObj = JSON.stringify(obj);
+console.log(stringyObj);
+
+console.log(JSON.parse(stringyObj));
